61ed76013f1eb3bdd31562f3bd0887a89df73a25
yellow-egg-a <yellow_egg_a@naver.com>
2023-01-15 20:11:52 +0900

    feature: Service 테스트 작성

--- dmaker/src/test/java/com/fastcampus/programming/dmaker/service/DMakerServiceTest.java
+++ dmaker/src/test/java/com/fastcampus/programming/dmaker/service/DMakerServiceTest.java
@@ -3,26 +3,27 @@ package com.fastcampus.programming.dmaker.service;
 import com.fastcampus.programming.dmaker.code.StatusCode;
 import com.fastcampus.programming.dmaker.dto.CreateDeveloper;
 import com.fastcampus.programming.dmaker.dto.DeveloperDetailDto;
-import com.fastcampus.programming.dmaker.dto.DeveloperDto;
 import com.fastcampus.programming.dmaker.entity.Developer;
+import com.fastcampus.programming.dmaker.exception.DMakerErrorCode;
+import com.fastcampus.programming.dmaker.exception.DMakerException;
 import com.fastcampus.programming.dmaker.repository.DeveloperRepository;
 import com.fastcampus.programming.dmaker.repository.RetiredDeveloperRepository;
-import com.fastcampus.programming.dmaker.type.DeveloperLevel;
-import com.fastcampus.programming.dmaker.type.DeveloperSkillType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
 import java.util.Optional;
 
+import static com.fastcampus.programming.dmaker.type.DeveloperLevel.SENIOR;
+import static com.fastcampus.programming.dmaker.type.DeveloperSkillType.FRONT_END;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 //@SpringBootTest // Integrated Test // 실행할 때와 동일한 환경으로 등록된 Bean을 다 띄워서 돌려보겠다!
 @ExtendWith(MockitoExtension.class) // Mockito라는 외부 툴을 이용해 테스트 하겠다!
@@ -38,24 +39,38 @@ class DMakerServiceTest {
     @InjectMocks // 가짜를 DMakerService에 Inject 해준다!
     private DMakerService dMakerService;
 
+    private final Developer defaultDeveloper = Developer.builder()
+            .developerLevel(SENIOR)
+            .developerSkillType(FRONT_END)
+            .experienceYears(12)
+            .statusCode(StatusCode.EMPLOYED)
+            .memberId("memberId")
+            .name("name")
+            .age(31)
+            .build();
+
+    private final CreateDeveloper.Request defaultCreateRequest = CreateDeveloper.Request.builder()
+            .developerLevel(SENIOR)
+            .developerSkillType(FRONT_END)
+            .experienceYears(12)
+            .memberId("memberId")
+            .name("name")
+            .age(32)
+            .build();
+
     @Test
     public void testSomething() {
+        //given
         // findByMemberId에 아무 문자열이나 주면 아래와 같이 응답하도록 mocking 해놓겠다!
         given(developerRepository.findByMemberId(anyString()))
-                .willReturn(Optional.of(Developer.builder()
-                        .developerLevel(DeveloperLevel.SENIOR)
-                        .developerSkillType(DeveloperSkillType.FRONT_END)
-                        .experienceYears(12)
-                        .statusCode(StatusCode.EMPLOYED)
-                        .memberId("memberId")
-                        .name("name")
-                        .age(31)
-                        .build()));
+                .willReturn(Optional.of(defaultDeveloper));
 
+        //when
         DeveloperDetailDto developerDetail = dMakerService.getDeveloperDetail("memberId");
 
-        assertEquals(DeveloperLevel.SENIOR, developerDetail.getDeveloperLevel());
-        assertEquals(DeveloperSkillType.FRONT_END, developerDetail.getDeveloperSkillType());
+        //then
+        assertEquals(SENIOR, developerDetail.getDeveloperLevel());
+        assertEquals(FRONT_END, developerDetail.getDeveloperSkillType());
         assertEquals(12, developerDetail.getExperienceYears());
     }
 
@@ -75,4 +90,43 @@ class DMakerServiceTest {
 //        System.out.println(allEmployedDevelopers);
 //        System.out.println("==========================");
 //    }
+
+    @Test
+    void createDeveloperTest_success() {
+        //given
+        given(developerRepository.findByMemberId(anyString()))
+                .willReturn(Optional.empty());
+
+        // 실제 저장된 데이터를 캡처해서 확인해볼 수 있다. (DB 저장 데이터 / API 파라미터 등 테스트 하고 싶을 때 사용)
+        ArgumentCaptor<Developer> captor = ArgumentCaptor.forClass(Developer.class);
+
+        //when
+        CreateDeveloper.Response developer = dMakerService.createDeveloper(defaultCreateRequest);
+
+        //then
+        verify(developerRepository, times(1))
+                .save(captor.capture()); // Mock인 developerRepository에 save한다.
+        Developer savedDeveloper = captor.getValue();
+        assertEquals(SENIOR, savedDeveloper.getDeveloperLevel());
+        assertEquals(FRONT_END, savedDeveloper.getDeveloperSkillType());
+        assertEquals(12, savedDeveloper.getExperienceYears());
+    }
+
+    @Test
+    void createDeveloperTest_failed_with_duplicated() {
+        //given
+        given(developerRepository.findByMemberId(anyString()))
+                .willReturn(Optional.of(defaultDeveloper));
+
+        // 실제 저장된 데이터를 캡처해서 확인해볼 수 있다.
+        ArgumentCaptor<Developer> captor = ArgumentCaptor.forClass(Developer.class);
+
+        //when
+        //then
+        DMakerException dMakerException = assertThrows(DMakerException.class,
+                () -> dMakerService.createDeveloper(defaultCreateRequest)
+        );
+
+        assertEquals(DMakerErrorCode.DUPLICATED_MEMBER_ID, dMakerException.getDMakerErrorCode());
+    }
 }
