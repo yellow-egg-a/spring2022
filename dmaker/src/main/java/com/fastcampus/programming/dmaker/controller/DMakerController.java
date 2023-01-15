bffc421b1a5f285152f15804b073355da7443f8f
yellow-egg-a <yellow_egg_a@naver.com>
2023-01-15 12:11:31 +0900

    feature: 글로벌 예외처리 기법_01

--- dmaker/src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
+++ dmaker/src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
@@ -1,13 +1,13 @@
 package com.fastcampus.programming.dmaker.controller;
 
-import com.fastcampus.programming.dmaker.dto.CreateDeveloper;
-import com.fastcampus.programming.dmaker.dto.DeveloperDetailDto;
-import com.fastcampus.programming.dmaker.dto.DeveloperDto;
-import com.fastcampus.programming.dmaker.dto.EditDeveloper;
+import com.fastcampus.programming.dmaker.dto.*;
+import com.fastcampus.programming.dmaker.exception.DMakerException;
 import com.fastcampus.programming.dmaker.service.DMakerService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -40,8 +40,7 @@ public class DMakerController {
 
     @PostMapping("/create-developer")
     public CreateDeveloper.Response createDevelopers(
-            @Valid @RequestBody CreateDeveloper.Request request )
-    {
+            @Valid @RequestBody CreateDeveloper.Request request) {
         log.info("request : {}", request);
 
         return dMakerService.createDeveloper(request);
@@ -58,5 +57,17 @@ public class DMakerController {
     @DeleteMapping("/developer/{memberId}")
     public DeveloperDetailDto deleteDeveloper(@PathVariable String memberId) {
         return dMakerService.deleteDeveloper(memberId);
+    }
+
+//    @ResponseStatus(value = HttpStatus.CONFLICT) // 설정 안 해주면 예외가 발생해도 응답이 200 OK로 감. But, 실무에서는 200 코드로 내려주고 아래 에러코드, 에러메시지를 변경하도록 개발함
+    @ExceptionHandler(DMakerException.class)
+    public DMakerErrorResponse handleException(DMakerException e, HttpServletRequest request) {
+        log.error("errorCode:  {}, url: {}, message: {}",
+                e.getDMakerErrorCode(), request.getRequestURI(), e.getDetailMessage());
+
+        return DMakerErrorResponse.builder()
+                .errorCode(e.getDMakerErrorCode())
+                .errorMessage(e.getDetailMessage())
+                .build();
     }
 }
---
+++ dmaker/src/main/java/com/fastcampus/programming/dmaker/exception/DMakerExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.fastcampus.programming.dmaker.exception;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice // 각 컨트롤러에 조언을 해주는 역할
+public class DMakerExceptionHandler {
+}
